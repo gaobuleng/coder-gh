3402b2f1f9214917817c5e17445eaaef10f67de8
gaobuleng <2463712878@qq.com>
2019-11-20 20:45:52 +0800

    fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,27 +1,13 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        fibonacci(5);
-        fibonacci(2);
-        fibonacci(15);
-    }
-
-    public static void fibonacci(int n) {
-        int a = 1;
-        int b = 1;
-        //if(n=1)
-
-        if (n > 1)
-            System.out.print(a + " ");
-        System.out.print(b + " ");
-        if (n > 2) {
-            for (int i = 2; i < n; i++) {
-                int c = a + b;
-                System.out.print(c + " ");
-                a = b;
-                b = c;
-            }
-
+        int a=0;
+        int b=1;
+        System.out.print(b+" ");
+        for(int i=1;i<10;i++){
+            int c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
         }
-        System.out.println();
     }
 }
