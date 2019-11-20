d941be2ddc3fcd1538b10c11290f1dafa90e2fd4
gaobuleng <2463712878@qq.com>
2019-11-20 13:27:55 +0800

    构造器初始化

---
+++ String_ini2.java
@@ -0,0 +1,18 @@
+public class String_ini2 {
+    public static void main(String[] args) {
+        String s1 = "initialize at definition";
+        String s2;
+        //String1 string1 = new String1();
+        new String1("ghhh");
+        System.out.println("s1= " + s1);
+        //System.out.println("s2= "+string1);
+    }
+}
+
+class String1 {
+    String1(String s1) {
+        //String s2;
+        //s2=str1;
+        System.out.println("s2= " + s1);
+    }
+}
