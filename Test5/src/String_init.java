4c44cc224845b091280c0d367af3c38cc35846d7
gaobuleng <2463712878@qq.com>
2019-11-20 13:43:58 +0800

    无初始化

---
+++ Test5/src/String_init.java
@@ -0,0 +1,11 @@
+public class String_init {
+    String s;
+
+    public static void main(String[] args) {
+        String_init string_init = new String_init();
+        //String string_init= new String();
+        System.out.println("s= " + string_init);
+        //System.out.println("str2: "+str2);
+    }
+}
+
