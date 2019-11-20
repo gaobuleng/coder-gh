bce7795183890c1c03ec2a7d6548067b7b28e30a
gaobuleng <2463712878@qq.com>
2019-11-20 20:15:00 +0800

    fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        System.out.print(b+" ");
+        for(int i=1;i<10;i++){
+            int c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+    }
+}
