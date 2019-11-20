ecf0c22f4434871a4f4bec58174a71fe218a3098
gaobuleng <2463712878@qq.com>
2019-11-20 21:25:38 +0800

    fibonacci

--- Test5/src/Fibonacci.java
+++ Test5/src/Fibonacci.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Fibonacci {
+    public static void main(String[] args) {
+        fibonacci(5);
+        fibonacci(2);
+        fibonacci(15);
+    }
+
+    public static void fibonacci(int n) {
+        int a = 1;
+        int b = 1;
+        if (n == 1)
+            System.out.print(a + " ");
+
+        if (n > 1)
+            System.out.print(a + " ");
+        System.out.print(b + " ");
+        if (n > 2) {
+            for (int i = 2; i < n; i++) {
+                int c = a + b;
+                System.out.print(c + " ");
+                a = b;
+                b = c;
+            }
+
+        }
+        System.out.println();
+    }
 }
