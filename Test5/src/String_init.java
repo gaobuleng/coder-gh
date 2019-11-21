7d85adae7332e96c0857919268c1040be129e4c0
gaobuleng <2463712878@qq.com>
2019-11-21 16:50:44 +0800

    string init

--- Test5/src/String_init.java
+++ Test5/src/String_init.java
@@ -1,10 +1,10 @@
 public class String_init {
-    String s;
+    String ss;
 
     public static void main(String[] args) {
         String_init string_init = new String_init();
         //String string_init= new String();
-        System.out.println("s= " + string_init);
+        System.out.println("ss= " + string_init);
         //System.out.println("str2: "+str2);
     }
 }
