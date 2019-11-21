3ead490a607a8489dfb458f0fdbf1bec6ab10801
gaobuleng <2463712878@qq.com>
2019-11-21 16:51:16 +0800

    string init

--- Test5/src/Fibonacci.java
+++ Test5/src/Fibonacci.java
@@ -1,3 +1,5 @@
+import java.math.*;
+
 public class Fibonacci {
     public static void main(String[] args) {
         fibonacci(5);
@@ -6,22 +8,26 @@ public class Fibonacci {
     }
 
     public static void fibonacci(int n) {
-        int a = 1;
-        int b = 1;
-        int c=0;
+        //int a = 1;
+        //int b = 1;
+        //int c = 0;
+        BigInteger a = BigInteger.valueOf(1);
+        BigInteger b = BigInteger.valueOf(1);
+        BigInteger c = BigInteger.valueOf(0);
         if (n == 1)
             System.out.print(a + " ");
 
         if (n == 2)
             //System.out.print(a + " ");
-             System.out.print(b + " ");
+            System.out.print(b + " ");
         if (n > 2) {
             for (int i = 2; i < n; i++) {
-                 c = a + b;
-               // System.out.print(c + " ");
+                c = a.add(b);
+                // System.out.print(c + " ");
                 a = b;
                 b = c;
             }
+            //BigInteger cc =BigInteger.valueOf(c);
             System.out.print(c + " ");
         }
         System.out.println();
