8adb2de3f57d9547376455c168f671d0298f9082
gaobuleng <2463712878@qq.com>
2019-11-24 20:33:39 +0800

    reverse

--- Test5/src/Overturn.java
+++ Test5/src/Overturn.java
@@ -1,19 +1,17 @@
 public class Overturn {
     public static void main(String[] args) {
-        reverse(10230);
+        reverse(12360);
     }
-    public static void reverse(int x) {
-        int ans = 0;
-        while (x != 0) {
-            int pop = x % 10;
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+    public static void reverse(int x){
+        long ans=0;
+        while(x!=0){
+            ans =  ans*10 + x%10;
+            if(ans>Integer.MAX_VALUE || ans<Integer.MIN_VALUE ){
                 System.out.println(0);
-            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
-                System.out.println(0);
-            ans = ans * 10 + pop;
-            x /= 10;
+            }
+            x/=10;
         }
-        System.out.println(ans);
+        System.out.println((int)ans);
     }
 }
 
