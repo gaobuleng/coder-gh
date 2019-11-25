ca64cf545bf2bf00e237572f63574e699ba21fb1
gaobuleng <2463712878@qq.com>
2019-11-25 20:18:47 +0800

    atoi

--- Test5/src/Atoi.java
+++ Test5/src/Atoi.java
@@ -3,23 +3,31 @@
 
 public class Atoi {
     public static void main(String[] args) {
-        String s = "ABCdfgh";
+        String s = "123";
         System.out.println(s);
         System.out.println(atoi(s));
     }
 
     public static int atoi(String s) {
-        int len = s.length(), ans = 0;
+        int len = s.length();
+        long ans = 0;
         char c;
         for (int i = 0; i < len; i++) {
             c = s.charAt(i);
             int ic = (int) c;
-            if (ans > Integer.MAX_VALUE / 10 || ans == Integer.MAX_VALUE / 10 && ic > 7) {
-                return 0;
+            if (ic / 100 > 0) {
+                ans = ans * 1000 + ic;
+            } else if (ic / 10 > 0) {
+                ans = ans * 100 + ic;
+            } else {
+                ans = ans * 10 + ic;
             }
-            ans = ans * 10 + ic;
+
         }
-        return ans;
+        if (ans > Integer.MAX_VALUE || ans<Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int)ans;
     }
 
 }
