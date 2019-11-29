f5a3312f59f97bfd399705aa55ede2b6552499fa
gaobuleng <2463712878@qq.com>
2019-11-29 12:47:54 +0800

    step

--- Test5/src/Step.java
+++ Test5/src/Step.java
@@ -1,17 +1,46 @@
-/*N级台阶，每次走一级或者两级  共有多少种方法*/
+/*N级台阶，每次走一级或者两级
+            共有多少种方法*/
 public class Step {
     public static void main(String[] args) {
-        System.out.println(stepNum(100));
+        System.out.println(stepNum(10));
     }
 
     public static int stepNum(int n) {
-        int sstep = 0, dstep = 0;
-        for (int i = 0; i < n; i++) {
-            sstep += 1;
+        long numstep = 0;
+        if (n % 2 == 0) {
+            for (int i = n / 2 + 1; i < n; ++i) {
+                if (numstep > Integer.MAX_VALUE || numstep < Integer.MIN_VALUE) {
+                    return 0;
+                }
+                numstep += numMid(i, n);
+            }
+            return (int) (numstep + 2);
+        } else {
+            for (int i = n / 2 + 2; i < n; ++i) {
+                if (numstep > Integer.MAX_VALUE || numstep < Integer.MIN_VALUE) {
+                    return 0;
+                }
+                numstep += numMid(i, n);
+            }
+            return (int) (numstep + n / 2 + 2);
         }
-        for (int i = 0; i < n; i += 2) {
-            dstep += 1;
-        }
-        return (sstep - dstep + 1);
+
     }
+
+    static int numMid(int i, int n) {
+        int res = 0;
+        res = n % i;
+        return ((factorial(i)) / (factorial(res)) / factorial(i - res));
+
+    }
+
+    static int factorial(int ii) {
+        int ans = 1;
+        for (int i = ii; i >= 1; --i) {
+            ans = ans * i;
+        }
+        return ans;
+    }
+
 }
+
