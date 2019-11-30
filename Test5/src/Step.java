fe9a0418c2736379c498305f83087ba6ee85e48e
gaobuleng <2463712878@qq.com>
2019-11-30 14:14:14 +0800

    step

--- Test5/src/Step.java
+++ Test5/src/Step.java
@@ -6,41 +6,21 @@ public class Step {
     }
 
     public static int stepNum(int n) {
-        long numstep = 0;
-        if (n % 2 == 0) {
-            for (int i = n / 2 + 1; i < n; ++i) {
-                if (numstep > Integer.MAX_VALUE || numstep < Integer.MIN_VALUE) {
-                    return 0;
-                }
-                numstep += numMid(i, n);
+         int a = 1,b=1,c=0;
+         if(n==0){
+             return a;
+         } else if(n==1){
+            return b;
+         }else{
+            for(int i=2;i<=n;i++){
+                c=a+b;
+                a=b;
+                b=c;
             }
-            return (int) (numstep + 2);
-        } else {
-            for (int i = n / 2 + 2; i < n; ++i) {
-                if (numstep > Integer.MAX_VALUE || numstep < Integer.MIN_VALUE) {
-                    return 0;
-                }
-                numstep += numMid(i, n);
-            }
-            return (int) (numstep + n / 2 + 2);
+            return c;
         }
 
     }
-
-    static long numMid(int i, int n) {
-        int res = 0;
-        res = n % i;
-        return ((factorial(i)) / (factorial(res)) / factorial(i - res));
-
-    }
-
-    static long factorial(int ii) {
-        long ans = 1L;
-        for (int i = ii; i >= 1; --i) {
-            ans = ans * i;
-        }
-        return ans;
-    }
-
 }
+
 
